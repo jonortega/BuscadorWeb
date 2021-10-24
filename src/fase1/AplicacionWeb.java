5ff9742adc2d073f6d072a55ab3e6c7aa66382f9
jonortega <jon.ortega.g@gmail.com>
2021-10-24 19:36:35 +0200

    Implementado la funcion principal de switch de AplicacionWeb

--- src/fase1/AplicacionWeb.java
+++ src/fase1/AplicacionWeb.java
@@ -29,7 +29,9 @@ public class AplicacionWeb {
 			System.out.println("0. Salir");
 			opcion = Integer.parseInt(sc.nextLine());
 			switch(opcion) {
-			case 1: //TO DO
+			case 1: System.out.println("Introduce una palabra: ");
+					pal = sc.nextLine();
+					internet.buscadorWeb(pal);
 				    break;
 			default: break;
 			}
